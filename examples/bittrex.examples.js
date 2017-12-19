e2b559187f530ea157f43bb8acebe15e2667f2e6
arminneman <arminsaraji@hotmail.com>
2017-12-19 02:46:45 +0100

    mysql queries fixed
    mysql queries fixed

--- examples/bittrex.examples.js
+++ examples/bittrex.examples.js
@@ -3,16 +3,17 @@ const APIKEY = 'KEY';
 const APISECRET = 'SECRET';
 var nodemailer = require('nodemailer');
 
+
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: '',
+        user: '@gmail.com',
         pass: ''
     }
 });
 
 
-var averages =  new Buffer
+var averages= "";
 
 
 
@@ -41,19 +42,27 @@ bittrex.options({
 
 function getAverage(market){
     var a= null;
-    con.query("select results.market, results.BV from results where MARKET = " + "'" +market +"'"+ "and T BETWEEN NOW() - INTERVAL 11 hour AND NOW() LIMIT 1", function (err, result, fields) {
+    con.query("select results.market, results.BV from results where MARKET = " + "'" +market +"'"+ "and T = SEC_TO_TIME((TIME_TO_SEC(NOW()) DIV 1800) * 1800) - INTERVAL 1 hour  ORDER BY T DESC LIMIT 1", function (err, result, fields) {
         if (err) throw err;
         a = result[0];
     });
 
-    con.query("select results.market, AVG(BV) from results where MARKET = " + "'" + market +"'"+ " and T BETWEEN NOW() - INTERVAL 11 hour AND NOW()-1 LIMIT 10", function (err, result, fields) {
+    con.query("select results.market, AVG(BV) from results where MARKET = " + "'" + market +"'"+ " and T BETWEEN SEC_TO_TIME((TIME_TO_SEC(NOW()) DIV 1800) * 1800) - INTERVAL 6 hour  AND (SEC_TO_TIME((TIME_TO_SEC(NOW()) DIV 1800) * 1800)) - INTERVAL 90 minute ORDER BY T DESC", function (err, result, fields) {
         if (err) throw err;
+        if ( typeof a !== 'undefined'){
+            // console.log(a['BV']);
+            // console.log(result[0]['AVG(BV)']);
+            if((a['BV'] / result[0]['AVG(BV)']) > 4 && (a['BV'] !== null && result[0]['AVG(BV)'] !==null)){
+                averages = (result[0]['market'] +": " +  (a['BV'] / result[0]['AVG(BV)']));
+                console.log("Alert");
+                sendMail(averages);
+                // generateReport(market);
+            }
 
-        if((a['BV'] / result[0]['AVG(BV)'] > 2) && (a['BV'] !== null && result[0]['AVG(BV)'] !==null) ){
-            averages.fill(result[0]['market'] +": " +  (a['BV'] / result[0]['AVG(BV)']));
-            console.log("Alert");
+            console.log(result[0]['market'] +": " +  (a['BV'] / result[0]['AVG(BV)']));
         }
-        // console.log(result[0]['market'] +": " +  (a['BV'] / result[0]['AVG(BV)']));
+
+
 
     });
 }
@@ -61,7 +70,7 @@ function getAverage(market){
 function persist(data, market) {
 
     if (data !== null) {
-        var sql = "INSERT INTO results (O, H, L, C, V, T, BV, Market) VALUES "
+        var sql = "INSERT IGNORE INTO results (O, H, L, C, V, T, BV, Market) VALUES "
         for (var i = data.length -1, len = data.length -120; i > len; i--) {
             if (i === data.length -1) {
                 sql += "(";
@@ -74,7 +83,7 @@ function persist(data, market) {
         // console.log(sql);
         con.query(sql, function (err) {
             if (err){
-                console.log(market + err);
+                console.log(market + ": " + err);
             }
             // console.log(market + " inserted");
         });
@@ -91,7 +100,7 @@ con.connect(function (err) {
 });
 
 
-for (var i = 0, len = 50; i < len; i++) {
+for (var i = 0, len = markets.length; i < len; i++) {
     (function () {
         var m = markets[i].toString().trim();
         var fullUrl = 'https://bittrex.com/Api/v2.0/pub/market/GetTicks?marketName=' + m + '&tickInterval=thirtyMin';
@@ -102,27 +111,26 @@ for (var i = 0, len = 50; i < len; i++) {
                     data = JSON.parse(data);
                     persist(data.result, m);
                     getAverage(m);
-                    if(i === 50 ){
-                        console.log("test");
-                        sendMail(averages);
-                            console.log("test");
-                    }
+                    // if(i === markets.length ){
+                    //     console.log("test");
+                    //     sendMail(averages);
+                    //         console.log("test");
+                    // }
                 }
             }, true
         );
     })();
 }
 
-var mailOptions = {
-    from: 'arminneman@gmail.com',
-    to: 'arminsaraji@gmail.com@',
-    subject: 'Sending Email using Node.js',
-    text: averages
-}
 
 function sendMail(averages){
 
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions = {
+        from: 'arminneman@gmail.com',
+        to: 'gutsu001@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: averages
+    }, function(error, info){
         if (error) {
             console.log(error);
         } else {
@@ -130,3 +138,4 @@ function sendMail(averages){
         }
     });
 }
+
